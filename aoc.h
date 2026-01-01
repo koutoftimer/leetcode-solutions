0c3ead6424627c659407cce59b5c92a9222aef0c
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-01 18:38:43 +0200

    Initial
    Adds some commoon things into solution.h.
    You are supposed to run tests in run.c and write definition in
    solution.c.

---
+++ aoc.h
@@ -0,0 +1,86 @@
+#ifndef __AOC_LIB_H__
+#define __AOC_LIB_H__
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi2,mmx,sse2,sse3,sse4")
+
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <setjmp.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <uthash.h>
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(*a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+#define typeof __typeof
+#define static_assert _Static_assert
+#define da_append(array, value)                                             \
+        do {                                                                \
+                static_assert(_Generic(array.size, size_t: 1, default: 0)); \
+                static_assert(                                              \
+                    _Generic(array.capacity, size_t: 1, default: 0));       \
+                static_assert(                                              \
+                    _Generic(*array.items, typeof(value): 1, default: 0));  \
+                if (array.items == NULL) {                                  \
+                        array.size     = 0;                                 \
+                        array.capacity = 10;                                \
+                        array.items =                                       \
+                            malloc(sizeof(*array.items) * array.capacity);  \
+                }                                                           \
+                if (array.size + 1 > array.capacity) {                      \
+                        array.capacity *= 2;                                \
+                        array.items =                                       \
+                            reallocarray(array.items, array.capacity,       \
+                                         sizeof(*array.items));             \
+                }                                                           \
+                array.items[array.size++] = value;                          \
+        } while (0)
+
+struct ListNode {
+        int val;
+        struct ListNode* next;
+};
+
+struct TreeNode {
+        int val;
+        struct TreeNode* left;
+        struct TreeNode* right;
+};
+
+static struct TreeNode*
+add_node_into_the_tree(struct TreeNode* root, int* value)
+{
+        (void)root;
+        (void)value;
+        return NULL;
+}
+
+[[gnu::unused]]
+static struct TreeNode*
+make_tree(int* values, int size)
+{
+        struct TreeNode* root = NULL;
+        for (int i = 0; i < size; ++i) {
+                add_node_into_the_tree(root, &values[i]);
+        }
+        return root;
+}
+
+typedef int (*Comparator)(const void*, const void*);
+
+// [[gnu::unused]]
+// static int
+// cmp(const int* a, const int* b)
+// {
+//         return a - b;
+// }
+
+#endif  // __AOC_LIB_H__
---
+++ build.sh
@@ -0,0 +1,23 @@
+# set -xe
+
+CC=gcc-14
+# CC=gcc-15
+# CC=clang-19
+# CC=clang-20
+
+# CFLAGS=(-I.. -Wall -Wextra -Wno-comment -ggdb3 -std=gnu11 -fsanitize=address)
+CFLAGS=(
+  -I.. -Wall -Wextra
+  -Wno-comment
+  -Wno-char-subscripts
+  -ggdb3
+  -fsanitize=address
+  -fsanitize=signed-integer-overflow
+)
+CFLAGS+=(-std=gnu11)
+# CFLAGS+=(-std=gnu23)
+# CFLAGS+=(-std=c23)
+# CFLAGS+=(-O2)
+CFLAGS+=(-O0)
+
+$CC ${CFLAGS[@]} run.c && time ./a.out
