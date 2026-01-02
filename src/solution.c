7e0f5a9cda5f824fee765bfd30948fb873890c60
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 05:02:52 +0200

    solved

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,19 @@
 #include "solution.h"
 
-// definition goes here
+static int
+impl(struct TreeNode* root, int* sum)
+{
+        if (!root) return 0;
+        root->val += impl(root->left, sum) + impl(root->right, sum);
+        *sum += abs((root->left ? root->left->val : 0) -
+                    (root->right ? root->right->val : 0));
+        return root->val;
+}
+
+int
+findTilt(struct TreeNode* root)
+{
+        int sum = 0;
+        impl(root, &sum);
+        return sum;
+}
