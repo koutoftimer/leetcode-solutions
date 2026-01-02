92dac16143a971d31174f927a82adae98f8e6c9d
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 02:16:09 +0200

    using pthread_once() to init dp

--- src/solution.c
+++ src/solution.c
@@ -1,8 +1,19 @@
 #include "solution.h"
 
-static const int dp[31] = {0,      1,      1,     2,     3,     5,      8,
-                           13,     21,     34,    55,    89,    144,    233,
-                           377,    610,    987,   1597,  2584,  4181,   6765,
-                           10946,  17711,  28657, 46368, 75025, 121393, 196418,
-                           317811, 514229, 832040};
-#define fib(n) dp[n]
+static int dp[31] = {0, 1};
+
+static void
+init_fib()
+{
+        for (int i = 2; i <= 30; ++i) {
+                dp[i] = dp[i - 2] + dp[i - 1];
+        }
+}
+
+int
+fib(int n)
+{
+        static pthread_once_t ONCE = PTHREAD_ONCE_INIT;
+        pthread_once(&ONCE, init_fib);
+        return dp[n];
+}
