7a76e4e930084b55b0b81db47c81ae21380a0e55
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 22:15:06 +0200

    solved without recursion

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,32 @@
 #include "solution.h"
 
-// definition goes here
+#define stack_push(value) stack[stack_size++] = (value)
+#define stack_pop() stack[--stack_size]
+#define stack_top() stack[stack_size - 1]
+
+int*
+postorder(struct Node* root, int* returnSize)
+{
+        int* res            = malloc(10001 * sizeof(*res));
+        int size            = 0;
+        struct Node** stack = malloc(10001 * sizeof(*stack));
+        int stack_size      = 0;
+        stack_push(root);
+        while (stack_size) {
+                struct Node* node = stack_pop();
+                if (!node) continue;
+                res[size++] = node->val;
+                for (int i = 0; i < node->numChildren; ++i) {
+                        stack_push(node->children[i]);
+                }
+        }
+        int *l = res, *r = res + size - 1;
+        while (l < r) {
+                int tmp = *l;
+                *l++    = *r;
+                *r--    = tmp;
+        }
+        *returnSize = size;
+        free(stack);
+        return res;
+}
