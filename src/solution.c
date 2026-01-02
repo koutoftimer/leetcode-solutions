99c2f1a40a63ee965834c394280eaacbafc923b9
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 02:09:20 +0200

    Solves using [[gnu::constructor]]

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,18 @@
 #include "solution.h"
 
-// definition goes here
+static int dp[31] = {0, 1};
+
+[[gnu::constructor]]
+static void
+init_fib()
+{
+        for (int i = 2; i <= 30; ++i) {
+                dp[i] = dp[i - 2] + dp[i - 1];
+        }
+}
+
+int
+fib(int n)
+{
+        return dp[n];
+}
