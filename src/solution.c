53812f227f101f580008db8809963f500662eaf7
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 04:11:29 +0200

    solved

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,15 @@
 #include "solution.h"
 
-// definition goes here
+bool
+checkRecord(char* s)
+{
+        int absent = 0;
+        int late   = 0;
+        while (*s) {
+                char current = *s++;
+                if (current == 'A') ++absent;
+                late = current == 'L' ? late + 1 : 0;
+                if (late == 3) return false;
+        }
+        return absent < 2;
+}
