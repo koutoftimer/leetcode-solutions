c578abaddf8f934b3134a4e6ffb4fada8d491534
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 08:21:18 +0200

    Recursion

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,32 @@
 #include "solution.h"
 
-// definition goes here
+static int
+get_size(struct Node* root)
+{
+        if (!root) return 0;
+        int size = 0;
+        for (int i = 0; i < root->numChildren; ++i) {
+                size += get_size(root->children[i]);
+        }
+        return 1 + root->numChildren + size;
+}
+
+static void
+traversal(struct Node* root, int* res, int* size)
+{
+        if (!root) return;
+        res[(*size)++] = root->val;
+        for (int i = 0; i < root->numChildren; ++i) {
+                traversal(root->children[i], res, size);
+        }
+}
+
+int*
+preorder(struct Node* root, int* returnSize)
+{
+        int size    = get_size(root);
+        int* res    = malloc(size * sizeof(*res));
+        *returnSize = 0;
+        traversal(root, res, returnSize);
+        return res;
+}
