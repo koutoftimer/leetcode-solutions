e30bcebb7ad28afc398080c430a139fb37b3efbd
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 05:34:48 +0200

    count sort to find unique

--- src/solution.c
+++ src/solution.c
@@ -1,20 +1,17 @@
 #include "solution.h"
 
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
-
-static int
-cmp(int* a, int* b)
-{
-        return *a - *b;
-}
-
 int
 distributeCandies(int* candyType, int candyTypeSize)
 {
-        qsort(candyType, candyTypeSize, sizeof *candyType, (__compar_fn_t)cmp);
-        int unique = 1;
-        for (int i = 1; i < candyTypeSize; ++i) {
-                unique += candyType[i] != candyType[i - 1];
+        bool* eaten = calloc(200005, sizeof *eaten);
+        int unique  = 0;
+        for (int i = 0; i < candyTypeSize; ++i) {
+                int index = candyType[i] + 100000;
+                if (!eaten[index]) {
+                        eaten[index] = true;
+                        unique++;
+                }
         }
+        free(eaten);
         return MIN(unique, candyTypeSize / 2);
 }
