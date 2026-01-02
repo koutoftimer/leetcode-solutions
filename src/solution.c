4ac33a569512a52052e9808d0c2cb490b0cb752b
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 05:34:18 +0200

    qsort to find unique

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,20 @@
 #include "solution.h"
 
-// definition goes here
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+static int
+cmp(int* a, int* b)
+{
+        return *a - *b;
+}
+
+int
+distributeCandies(int* candyType, int candyTypeSize)
+{
+        qsort(candyType, candyTypeSize, sizeof *candyType, (__compar_fn_t)cmp);
+        int unique = 1;
+        for (int i = 1; i < candyTypeSize; ++i) {
+                unique += candyType[i] != candyType[i - 1];
+        }
+        return MIN(unique, candyTypeSize / 2);
+}
