edc2c84513d0da0bfa0eba426450bd5f8d947640
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 04:02:50 +0200

    solved

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,20 @@
 #include "solution.h"
 
-// definition goes here
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+char*
+reverseStr(char* res, int k)
+{
+        char* s = res;
+        while (*s) {
+                int len = strlen(s);
+                char *l = s, *r = s + MIN(len, k) - 1;
+                while (l < r) {
+                        char tmp = *l;
+                        *l++     = *r;
+                        *r--     = tmp;
+                }
+                s += MIN(2 * k, len);
+        }
+        return res;
+}
