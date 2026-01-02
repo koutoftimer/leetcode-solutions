a3617394f0a60e1661bc47d744d3b23f703bfbed
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 22:31:42 +0200

    recursion with little allocation

--- src/solution.c
+++ src/solution.c
@@ -1,32 +1,32 @@
 #include "solution.h"
 
-#define stack_push(value) stack[stack_size++] = (value)
-#define stack_pop() stack[--stack_size]
-#define stack_top() stack[stack_size - 1]
+static int
+get_size(struct Node* root)
+{
+        if (!root) return 0;
+        int sum = 1;
+        for (int i = 0; i < root->numChildren; ++i) {
+                sum += get_size(root->children[i]);
+        }
+        return sum;
+}
+
+static void
+traverse(struct Node* root, int* res, int* size)
+{
+        if (!root) return;
+        for (int i = 0; i < root->numChildren; ++i) {
+                traverse(root->children[i], res, size);
+        }
+        res[(*size)++] = root->val;
+}
 
 int*
 postorder(struct Node* root, int* returnSize)
 {
-        int* res            = malloc(10001 * sizeof(*res));
-        int size            = 0;
-        struct Node** stack = malloc(10001 * sizeof(*stack));
-        int stack_size      = 0;
-        stack_push(root);
-        while (stack_size) {
-                struct Node* node = stack_pop();
-                if (!node) continue;
-                res[size++] = node->val;
-                for (int i = 0; i < node->numChildren; ++i) {
-                        stack_push(node->children[i]);
-                }
-        }
-        int *l = res, *r = res + size - 1;
-        while (l < r) {
-                int tmp = *l;
-                *l++    = *r;
-                *r--    = tmp;
-        }
+        int* res = malloc(get_size(root) * sizeof(*res));
+        int size = 0;
+        traverse(root, res, &size);
         *returnSize = size;
-        free(stack);
         return res;
 }
