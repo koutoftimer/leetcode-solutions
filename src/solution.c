bb82b5ace6859a35f05367b4762509935eff37c7
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-02 05:15:59 +0200

    solved

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,20 @@
 #include "solution.h"
 
-// definition goes here
+bool
+same(struct TreeNode* root, struct TreeNode* subRoot)
+{
+        if (!root && !subRoot) return true;
+        if (!root || !subRoot) return false;
+        if (root->val != subRoot->val) return false;
+        return same(root->left, subRoot->left) &&
+               same(root->right, subRoot->right);
+}
+
+bool
+isSubtree(struct TreeNode* root, struct TreeNode* subRoot)
+{
+        if (!root) return false;
+        if (same(root, subRoot)) return true;
+        return isSubtree(root->left, subRoot) ||
+               isSubtree(root->right, subRoot);
+}
