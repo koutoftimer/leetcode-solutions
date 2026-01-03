3c575120feab02f9b6f10f7dd95dd1db3d05c40b
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-04 01:08:38 +0200

    WIP

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,21 @@
 #include "solution.h"
 
-// definition goes here
+int
+findLHS(int* a, int size)
+{
+        int cmp(int* a, int* b) { return *a - *b; }
+        qsort(a, size, sizeof *a, (__compar_fn_t)cmp);
+        int len = 0, prev_len = 0, max_len = len;
+        int *cur = a, *prev = a;
+        while (cur < a + size) {
+                while (cur < a + size && *cur++ == *prev) {
+                        ++len;
+                }
+                if (cur < a + size) {
+                        if (len > max_len) max_len = len;
+                        prev = cur;
+                        len  = 0;
+                }
+        }
+        return max_len;
+}
