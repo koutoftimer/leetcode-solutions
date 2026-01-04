3c8206dfc197deb73c8919b84ec7a04ab528b3e1
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-04 18:13:50 +0200

    TLE with custom hash

--- src/solution.c
+++ src/solution.c
@@ -1,24 +1,53 @@
 #include "solution.h"
 
+enum { BUCKETS_COUNT = 32768 };
+
+struct Bucket {
+        int key;
+        int count;
+};
+
+static inline uint
+hash(int key)
+{
+        return key * 2654435761u & (BUCKETS_COUNT - 1);
+}
+
+static inline int
+find(struct Bucket* map, int const key)
+{
+        int index = hash(key);
+        while (map[index].count && map[index].key != key) {
+                index = (index + 1) & (BUCKETS_COUNT & 1);
+        }
+        return index;
+}
+
 int
 findLHS(int* a, int size)
 {
         // asm("int3");
-        int cmp(int* a, int* b) { return *a - *b; }
-        qsort(a, size, sizeof *a, (__compar_fn_t)cmp);
-        int left = 0, right = 0;
-        int prev_value, prev_count;
-        int max = 0;
-        while (right < size) {
-                while (right < size && a[left] == a[right]) ++right;
-                int const count = right - left;
-                if (left && abs(prev_value - a[left]) == 1 &&
-                    prev_count + count > max) {
-                        max = prev_count + count;
+        struct Bucket map[BUCKETS_COUNT] = {0};
+        int max                          = 0;
+        for (int i = 0; i < size; ++i) {
+                int current      = find(map, a[i]);
+                map[current].key = a[i];
+                map[current].count++;
+                int prev = find(map, a[i] - 1);
+                if (map[prev].count &&
+                    map[current].count + map[prev].count > max) {
+                        max = map[current].count + map[prev].count;
                 }
-                prev_value = a[left];
-                prev_count = count;
-                left       = right;
+                int next = find(map, a[i] + 1);
+                if (map[next].count &&
+                    map[current].count + map[next].count > max) {
+                        max = map[current].count + map[next].count;
+                }
+                printf(
+                    "a[%d] = %d, current: %d->%d, prev: %d->%d, next: %d->%d\n",
+                    i, a[i], map[current].key, map[current].count,
+                    map[prev].key, map[prev].count, map[next].key,
+                    map[next].count);
         }
         return max;
 }
