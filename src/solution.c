7877434b817996c64f5ce85341e118f0a20b5cc6
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-04 18:30:41 +0200

    solved

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,13 @@
 #include "solution.h"
 
-// definition goes here
+int
+maxCount(int m, int n, int** ops, int opsSize, int* opsColSize)
+{
+        int min_x = m;
+        int min_y = n;
+        for (int i = 0; i < opsSize; ++i) {
+                if (ops[i][0] < min_x) min_x = ops[i][0];
+                if (ops[i][1] < min_y) min_y = ops[i][1];
+        }
+        return min_x * min_y;
+}
