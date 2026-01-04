6289c475ec0dbd915be254b0a9230a55b9673dd2
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 01:44:58 +0200

    qsort solution: O(NlogN) time O(1) space

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,21 @@
 #include "solution.h"
 
-// definition goes here
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(*a))
+
+int
+maximumProduct(int *a, int size)
+{
+        int cmp(int *a, int *b) { return *a - *b; }
+        qsort(a, size, sizeof *a, (__compar_fn_t)cmp);
+        int variations[] = {
+            a[0] * a[1] * a[2],
+            a[0] * a[1] * a[size - 1],
+            a[0] * a[size - 1] * a[size - 2],
+            a[size - 1] * a[size - 2] * a[size - 3],
+        };
+        int max = variations[0];
+        for (uint i = 1; i < ARRAY_SIZE(variations); ++i) {
+                if (max < variations[i]) max = variations[i];
+        }
+        return max;
+}
--- src/solution.h
+++ src/solution.h
@@ -93,4 +93,4 @@ struct Node {
 // Leetcode declaration goes below
 //
 
-// declaration goes here
+int maximumProduct(int* nums, int numsSize);
