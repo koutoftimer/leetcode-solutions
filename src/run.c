98bb0da8942f25e3d9d151b8a10cab6a79354979
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-04 10:07:04 +0200

    WIP

--- src/run.c
+++ src/run.c
@@ -3,4 +3,13 @@
 int
 main()
 {
+#define test(exp, ...)                                       \
+        do {                                                 \
+                int a[] = {__VA_ARGS__};                     \
+                int res = sumFourDivisors(a, ARRAY_SIZE(a)); \
+                printf("res: %d\n", res);                    \
+                assert(res == exp);                          \
+        } while (0)
+        // test(32, 21, 4, 7);
+        test(45, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 }
--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,42 @@
 #include "solution.h"
 
-// definition goes here
+int primes[65];
+int primes_len = 0;
+
+[[gnu::constructor]]
+static void
+build_primes()
+{
+        primes[primes_len++] = 2;
+        for (int i = 3; i * i <= 100000; i += 2) {
+                for (int j = 0; j < primes_len; ++j) {
+                        if (i % primes[j] == 0) goto not_prime;
+                }
+                primes[primes_len++] = i;
+        not_prime:
+        }
+}
+
+int
+sumFourDivisors(int* a, int size)
+{
+        // asm("int3");
+        int sum = 0;
+        for (int i = 0; i < size; ++i) {
+                int const number = a[i];
+                int cur_sum      = 0;
+                for (int j = 0;
+                     j < primes_len && primes[j] * primes[j] < number;  //
+                     ++j) {
+                        int const prime = primes[j];
+                        if (number % prime != 0) continue;
+                        int const remainder = number / prime;
+                        if (remainder == prime) continue;
+                        if (cur_sum) goto dont_add;
+                        cur_sum = 1 + prime + remainder + number;
+                }
+                sum += cur_sum;
+        dont_add:
+        }
+        return sum;
+}
