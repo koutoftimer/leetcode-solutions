560ffa33072927e3721370d87eb94df1ff3fa0fb
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-04 16:18:52 +0200

    qsort + counting into separate array

--- src/solution.c
+++ src/solution.c
@@ -5,17 +5,24 @@ findLHS(int* a, int size)
 {
         int cmp(int* a, int* b) { return *a - *b; }
         qsort(a, size, sizeof *a, (__compar_fn_t)cmp);
-        int len = 0, prev_len = 0, max_len = len;
-        int *cur = a, *prev = a;
-        while (cur < a + size) {
-                while (cur < a + size && *cur++ == *prev) {
-                        ++len;
-                }
-                if (cur < a + size) {
-                        if (len > max_len) max_len = len;
-                        prev = cur;
-                        len  = 0;
+        struct {
+                int value;
+                int count;
+        }* stack       = malloc(size * sizeof(*stack));
+        int stack_size = 0;
+        for (int i = 0; i < size; ++i) {
+                if (stack_size && stack[stack_size - 1].value == a[i]) {
+                        stack[stack_size - 1].count += 1;
+                } else {
+                        stack[stack_size].value   = a[i];
+                        stack[stack_size++].count = 1;
                 }
         }
-        return max_len;
+        int max = 0;
+        for (int i = 1; i < stack_size; ++i) {
+                int const diff = stack[i].value - stack[i - 1].value;
+                int const len  = stack[i].count + stack[i - 1].count;
+                if (abs(diff) == 1 && len > max) max = len;
+        }
+        return max;
 }
