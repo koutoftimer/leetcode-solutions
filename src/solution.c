b7c3333d7725140f58ebb82c89a8aa8d49c3ba43
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-04 18:50:12 +0200

    O(M*N^2)

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,21 @@
 #include "solution.h"
 
-// definition goes here
+char**
+findRestaurant(char** a, int a_len, char** b, int b_len, int* returnSize)
+{
+        char** res = malloc((a_len > b_len ? b_len : a_len) * sizeof *res);
+        int size   = 0;
+        int sum    = a_len + b_len;
+        for (int i = 0; i < a_len; ++i) {
+                for (int j = 0; j < b_len; ++j) {
+                        if (i + j > sum || strcmp(a[i], b[j]) != 0) continue;
+                        if (i + j < sum) {
+                                sum  = i + j;
+                                size = 0;
+                        }
+                        res[size++] = a;
+                }
+        }
+        *returnSize = size;
+        return res;
+}
