a6f5615f2bccbb535ef36e9e06bbb8e438092949
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 01:55:09 +0200

    counting sort: O(N) time

--- src/solution.c
+++ src/solution.c
@@ -5,8 +5,24 @@
 int
 maximumProduct(int *a, int size)
 {
-        int cmp(int *a, int *b) { return *a - *b; }
-        qsort(a, size, sizeof *a, (__compar_fn_t)cmp);
+        int count[2005] = {0};
+        for (int i = 0; i < size; ++i) {
+                count[a[i] + 1000]++;
+        }
+        for (int n = 3, i = 0; n && i < ARRAY_SIZE(count); ++i) {
+                while (n && count[i]) {
+                        a[3 - n] = i - 1000;
+                        --n;
+                        --count[i];
+                }
+        }
+        for (int n = 3, i = ARRAY_SIZE(count) - 1; n && i >= 0; --i) {
+                while (n && count[i]) {
+                        a[size - 1 + n - 3] = i - 1000;
+                        --n;
+                        --count[i];
+                }
+        }
         int variations[] = {
             a[0] * a[1] * a[2],
             a[0] * a[1] * a[size - 1],
