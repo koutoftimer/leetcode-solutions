734685fb6adce3d2386a5a39454b63e986898c5d
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 11:35:25 +0200

    O(N^2) time, O(1) space:
    1. Because when we moving from one cell to another we flip each
    intermediate cell twice, we don't change its value.
    
    2. If you we can flip any two values, we should be geady and pick cells
    with the highest absolute value, so that the least absolute value can
    stay negative.

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,21 @@
 #include "solution.h"
 
-// definition goes here
+long long
+maxMatrixSum(int** matrix, int matrixSize, int* matrixColSize)
+{
+        long long sum = 0;
+        int min       = INT_MAX;
+        int negatives = 0;
+        for (int i = 0; i < matrixSize; ++i) {
+                for (int j = 0; j < matrixSize; ++j) {
+                        int const value = matrix[i][j];
+                        sum += abs(value);
+                        negatives ^= 1;
+                        if (abs(value) < min) min = abs(value);
+                }
+        }
+        if (negatives) {
+                sum -= 2 * min;
+        }
+        return sum;
+}
