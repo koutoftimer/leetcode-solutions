3ca85b1dded5c04bd7ab99f03ba7968555f57bf5
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 12:49:34 +0200

    O(NlogN) time O(1) space (excluding O(logN) stack):
    `tree_contains` gets unrolled into the loop but `traverse` has to split
    on each node and saves current state into call stack with has length
    of the depth of the BST

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,22 @@
 #include "solution.h"
 
-// definition goes here
+static bool
+tree_contains(struct TreeNode* root, struct TreeNode* skip, int value)
+{
+        if (!root) return false;
+        if (root != skip && root->val == value) return true;
+        if (value < root->val) return tree_contains(root->left, skip, value);
+        return tree_contains(root->right, skip, value);
+}
+
+bool
+findTarget(struct TreeNode* root, int k)
+{
+        bool traverse(struct TreeNode * current)
+        {
+                if (!current) return false;
+                return tree_contains(root, current, k - current->val) ||
+                       traverse(current->left) || traverse(current->right);
+        }
+        return traverse(root);
+}
