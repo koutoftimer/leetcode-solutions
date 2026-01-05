be1d22e827ba983aeee685d73edd00f2c50f23de
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 13:11:52 +0200

    O(N) time O(M) space: using count sort
    Where N - number of nodes
          M - range of values
    
    This one is not that good with low depth trees because of requirement to
    reset the count sort array.

--- src/solution.c
+++ src/solution.c
@@ -1,22 +1,39 @@
 #include "solution.h"
 
-static bool
-tree_contains(struct TreeNode* root, struct TreeNode* skip, int value)
+enum { CAPACITY = (int)2e4 + 1, NORMALIZATION = (int)1e4 };
+
+bool* count;
+
+[[gnu::constructor]]
+static void
+init_count()
 {
-        if (!root) return false;
-        if (root != skip && root->val == value) return true;
-        if (value < root->val) return tree_contains(root->left, skip, value);
-        return tree_contains(root->right, skip, value);
+        count = malloc(CAPACITY * sizeof *count);
 }
 
-bool
+[[gnu::destructor]]
+static void
+free_count()
+{
+        free(count);
+}
+
+static bool
+impl(struct TreeNode* root, int k)
+{
+        if (!root) return false;
+        int const to_find = k - root->val + NORMALIZATION;
+        if (0 <= to_find && to_find < CAPACITY && count[to_find]) {
+                return true;
+        }
+        int const current = root->val + NORMALIZATION;
+        count[current]    = true;
+        return impl(root->left, k) || impl(root->right, k);
+}
+
+static bool
 findTarget(struct TreeNode* root, int k)
 {
-        bool traverse(struct TreeNode * current)
-        {
-                if (!current) return false;
-                return tree_contains(root, current, k - current->val) ||
-                       traverse(current->left) || traverse(current->right);
-        }
-        return traverse(root);
+        memset(count, 0, CAPACITY);
+        return impl(root, k);
 }
