702fc6156e880d702072a1084acff2e42be688a5
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 13:36:40 +0200

    O(N) time O(1) space: pattern matching array

--- src/solution.c
+++ src/solution.c
@@ -1,25 +1,24 @@
 #include "solution.h"
 
+struct {
+        int dx;
+        int dy;
+} changes[] = {
+    ['L'] = {.dx = -1},
+    ['R'] = {.dx = 1},
+    ['D'] = {.dy = -1},
+    ['U'] = {.dy = 1},
+};
+
 bool
 judgeCircle(char* moves)
 {
         int dx = 0;
         int dy = 0;
         while (*moves) {
-                switch (*moves++) {
-                case 'R': {
-                        dx++;
-                } break;
-                case 'L': {
-                        dx--;
-                } break;
-                case 'U': {
-                        dy++;
-                } break;
-                case 'D': {
-                        dy--;
-                } break;
-                }
+                char const move = *moves++;
+                dx += changes[move].dx;
+                dy += changes[move].dy;
         }
         return !dx && !dy;
 }
