c1fad32da7bbba20ec98fee0be43fa70cb25288c
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 21:43:44 +0200

    BitSet isnstead of HashSet. O(1) time and O(M) space
    M - range of values
    
    Just allocated memory for full range of values.

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,51 @@
 #include "solution.h"
 
-// definition goes here
+enum { CAPACITY = (int)1e6 + 1 };
+
+typedef struct {
+        bool* count;
+} MyHashSet;
+
+MyHashSet*
+myHashSetCreate()
+{
+        MyHashSet* self = malloc(sizeof *self);
+        self->count     = calloc(CAPACITY, sizeof *self->count);
+        return self;
+}
+
+void
+myHashSetAdd(MyHashSet* self, int key)
+{
+        self->count[key] = true;
+}
+
+void
+myHashSetRemove(MyHashSet* self, int key)
+{
+        self->count[key] = false;
+}
+
+bool
+myHashSetContains(MyHashSet* self, int key)
+{
+        return self->count[key];
+}
+
+void
+myHashSetFree(MyHashSet* self)
+{
+        free(self->count);
+}
+
+/**
+ * Your MyHashSet struct will be instantiated and called as such:
+ * MyHashSet* obj = myHashSetCreate();
+ * myHashSetAdd(obj, key);
+
+ * myHashSetRemove(obj, key);
+
+ * bool param_3 = myHashSetContains(obj, key);
+
+ * myHashSetFree(obj);
+*/
