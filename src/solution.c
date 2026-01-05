bd02a0fd0703d156412c75138af6f91d68b64a5e
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 15:12:57 +0200

    O(N) time O(1) space: double pass

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,23 @@
 #include "solution.h"
 
-// definition goes here
+static bool
+palindrom(char *l, char *r)
+{
+        while (l < r) {
+                if (*l++ != *r--) return false;
+        }
+        return true;
+}
+
+bool
+validPalindrome(char *s)
+{
+        int len = strlen(s);
+        char *l = s, *r = s + len - 1;
+        for (; l < r; ++l, --r) {
+                if (*l != *r) {
+                        return palindrom(l + 1, r) || palindrom(l, r - 1);
+                }
+        }
+        return true;
+}
