8b1b5cbceeaad5fcb29d5fe301d809b4faa2969f
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 11:52:44 +0200

    O(N) time O(N) space:
    Simple 2 pass solution.
    
    I'm calculating sum and count as integers to reduce amount of operations
    with doubles.

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,37 @@
 #include "solution.h"
 
-// definition goes here
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+static int
+tree_depth(struct TreeNode* root, int level)
+{
+        if (!root) return level;
+        int const left  = tree_depth(root->left, level + 1);
+        int const right = tree_depth(root->right, level + 1);
+        return MAX(left, right);
+}
+
+double*
+averageOfLevels(struct TreeNode* root, int* returnSize)
+{
+        int depth = tree_depth(root, 0);
+        long sum[depth];
+        int count[depth];
+        memset(sum, 0, sizeof(sum));
+        memset(count, 0, sizeof(count));
+        void traverse(struct TreeNode * root, int level)
+        {
+                if (!root) return;
+                sum[level] += root->val;
+                count[level]++;
+                traverse(root->left, level + 1);
+                traverse(root->right, level + 1);
+        }
+        traverse(root, 0);
+        double* res = malloc(depth * sizeof *res);
+        for (int i = 0; i < depth; ++i) {
+                res[i] = (double)sum[i] / count[i];
+        }
+        *returnSize = depth;
+        return res;
+}
