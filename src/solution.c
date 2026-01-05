1873df31bd88f5054c885230ee5ebb0c090fa01e
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 16:26:38 +0200

    solved

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,20 @@
 #include "solution.h"
 
-// definition goes here
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+int
+countBinarySubstrings(char* s)
+{
+        int sum      = 0;
+        int prev_len = 0;
+        char *l = s, *r = s;
+        while (*r) {
+                while (*r && *l == *r) r++;
+                int const len = r - l;
+                l             = r;
+                if (prev_len) {
+                        sum += MIN(len, prev_len);
+                }
+                prev_len = len;
+        }
+        return sum;
+}
