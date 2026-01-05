880c16f68de62657def1901b44b1b6e0d5e55139
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 17:11:15 +0200

    O(N) using count sort with metadata, double pass

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,49 @@
 #include "solution.h"
 
-// definition goes here
+enum { CAPACITY = 50000 };
+
+struct {
+        int first;
+        int last;
+        int freq;
+}* count;
+
+[[gnu::constructor]]
+static void
+init_count()
+{
+        count = malloc(CAPACITY * sizeof *count);
+}
+
+[[gnu::destructor]]
+static void
+free_count()
+{
+        free(count);
+}
+
+int
+findShortestSubArray(int* a, int size)
+{
+        memset(count, 0, CAPACITY * sizeof *count);
+
+        int max_freq = 0;
+        int min_len  = size - 1;
+        for (int i = 0; i < size; ++i) {
+                int const index = a[i];
+                count[index].freq++;
+                count[index].last = i + 1;
+                if (!count[index].first) count[index].first = i + 1;
+
+                int const len = count[index].last - count[index].first;
+                if (count[index].freq > max_freq) {
+                        max_freq = count[index].freq;
+                        min_len  = len;
+
+                } else if (count[index].freq == max_freq && len < min_len) {
+                        min_len = len;
+                }
+        }
+
+        return min_len + 1;
+}
