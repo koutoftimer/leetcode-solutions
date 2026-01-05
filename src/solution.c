a2f38f7ff2c98ed24b1e870cf81eae06e9f9a28b
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 14:37:03 +0200

    O(N) time O(1) space: dfs

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,22 @@
 #include "solution.h"
 
-// definition goes here
+int
+findSecondMinimumValue(struct TreeNode* root)
+{
+        int min    = root->val;
+        int second = -1;
+        void traverse(struct TreeNode * root)
+        {
+                if (!root) return;
+                if (root->val > min) {
+                        if (second == -1 || root->val < second) {
+                                second = root->val;
+                        }
+                        return;
+                }
+                traverse(root->left);
+                traverse(root->right);
+        }
+        traverse(root);
+        return second;
+}
