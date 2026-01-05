f036a596cf46b8114d2ccae09170b2f10ed9e119
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 14:42:20 +0200

    O(N) time O(1) space

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,16 @@
 #include "solution.h"
 
-// definition goes here
+int
+findLengthOfLCIS(int* a, int size)
+{
+        int len = 1, max = len;
+        for (int i = 1; i < size; ++i) {
+                if (a[i] > a[i - 1]) {
+                        len++;
+                        if (len > max) max = len;
+                } else {
+                        len = 1;
+                }
+        }
+        return max;
+}
