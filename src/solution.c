c5c85fa97962ba6e6400d4ca8fa73ee3afd5b84f
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 12:22:01 +0200

    O(N) time O(1) space: sliding window

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,15 @@
 #include "solution.h"
 
-// definition goes here
+double
+findMaxAverage(int *a, int size, int k)
+{
+        int sum = 0;
+        int i   = 0;
+        for (; i < k; ++i) sum += a[i];
+        int max = sum;
+        for (; i < size; ++i) {
+                sum = sum - a[i - k] + a[i];
+                if (sum > max) max = sum;
+        }
+        return (double)max / k;
+}
