636a549ebe5ac3b8ef539944b6cfde6fa067ee30
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-05 13:34:54 +0200

    O(N) time O(1) space: switch case

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,25 @@
 #include "solution.h"
 
-// definition goes here
+bool
+judgeCircle(char* moves)
+{
+        int dx = 0;
+        int dy = 0;
+        while (*moves) {
+                switch (*moves++) {
+                case 'R': {
+                        dx++;
+                } break;
+                case 'L': {
+                        dx--;
+                } break;
+                case 'U': {
+                        dy++;
+                } break;
+                case 'D': {
+                        dy--;
+                } break;
+                }
+        }
+        return !dx && !dy;
+}
