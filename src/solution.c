4d16ee5ae376ec90122e8601e8ab3f249eddc3ca
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-06 16:54:34 +0200

    solved: brute force

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,25 @@
 #include "solution.h"
 
-// definition goes here
+static bool
+is_self_dividing(int const number)
+{
+        int n = number;
+        while (n) {
+                int const digit = n % 10;
+                n /= 10;
+                if (!digit || number % digit) return false;
+        }
+        return true;
+}
+
+int*
+selfDividingNumbers(int left, int right, int* returnSize)
+{
+        int* res = malloc((right - left + 1) * sizeof *res);
+        int size = 0;
+        for (int number = left; number <= right; ++number) {
+                if (is_self_dividing(number)) res[size++] = number;
+        }
+        *returnSize = size;
+        return res;
+}
