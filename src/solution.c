f2c22ccd9b2c644279b9928569ef3f8293246d00
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-06 18:20:57 +0200

    O(logN) time O(1) space: binary search

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,17 @@
 #include "solution.h"
 
-// definition goes here
+char
+nextGreatestLetter(char* s, int size, char target)
+{
+        int low  = -1;
+        int high = size;
+        while (low + 1 < high) {
+                int const mid = low + (high - low) / 2;
+                if (s[mid] > target) {
+                        high = mid;
+                } else {
+                        low = mid;
+                }
+        }
+        return high == size ? *s : s[high];
+}
