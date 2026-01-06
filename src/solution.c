7721eb60021193a7ebcc31f730ab98d58632f81a
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-06 16:43:26 +0200

    O(N) time O(1) space: inplace partial sums

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,16 @@
 #include "solution.h"
 
-// definition goes here
+int
+findMiddleIndex(int* a, int size)
+{
+        // build partial sums inplace
+        for (int i = 1; i < size; ++i) {
+                a[i] += a[i - 1];
+        }
+        for (int i = 0; i < size; ++i) {
+                int const left  = i ? a[i - 1] : 0;
+                int const right = a[size - 1] - a[i];
+                if (left == right) return i;
+        }
+        return -1;
+}
