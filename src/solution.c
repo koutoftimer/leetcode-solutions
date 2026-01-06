7c3ab3db1c9bea0d604956ff11a2245266f8606e
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-06 19:50:51 +0200

    brute force using __builtin_popcount()

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,28 @@
 #include "solution.h"
 
-// definition goes here
+static int
+is_prime(int const value)
+{
+        switch (value) {
+        case 2:
+        case 3:
+        case 5:
+        case 7:
+        case 11:
+        case 13:
+        case 17:
+        case 19:
+                return true;
+        }
+        return false;
+}
+
+int
+countPrimeSetBits(int left, int right)
+{
+        int count = 0;
+        for (int i = left; i <= right; ++i) {
+                if (is_prime(__builtin_popcount(i))) count++;
+        }
+        return count;
+}
