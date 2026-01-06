7e01761f8370f76379c913f0109b1f095189a66b
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-06 19:02:42 +0200

    O(N) time O(1) space: 2 indexes

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,21 @@
 #include "solution.h"
 
-// definition goes here
+int
+dominantIndex(int* a, int size)
+{
+        int l = 0, r = 1;
+        if (a[r] < a[l]) {
+                int tmp = l;
+                l       = r;
+                r       = tmp;
+        }
+        for (int i = 2; i < size; ++i) {
+                if (a[i] > a[r]) {
+                        l = r;
+                        r = i;
+                } else if (a[i] > a[l]) {
+                        l = i;
+                }
+        }
+        return a[r] >= 2 * a[l] ? r : -1;
+}
