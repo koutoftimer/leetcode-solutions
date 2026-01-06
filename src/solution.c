eb9ea3b4e7d6da233adfdff537893ecdc7a750e6
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-06 20:26:00 +0200

    naive random access comparison

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,25 @@
 #include "solution.h"
 
-// definition goes here
+bool
+isToeplitzMatrix(int** a, int rows, int* cols)
+{
+        bool matches(int row, int col)
+        {
+                int const original = a[row++][col++];
+                while (row < rows && col < *cols) {
+                        if (a[row][col] != original) {
+                                return false;
+                        }
+                }
+                return true;
+        }
+        int col = 0, row;
+        for (row = 1; row < rows; ++row) {
+                if (!matches(row, col)) return false;
+        }
+        row = 0;
+        for (col = 0; col < *cols; ++col) {
+                if (!matches(row, col)) return false;
+        }
+        return true;
+}
