81bced155f3990392ab59b1c3cf6e04368921cfe
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-06 20:30:16 +0200

    compare row by row using memcmp

--- src/solution.c
+++ src/solution.c
@@ -3,23 +3,10 @@
 bool
 isToeplitzMatrix(int** a, int rows, int* cols)
 {
-        bool matches(int row, int col)
-        {
-                int const original = a[row++][col++];
-                while (row < rows && col < *cols) {
-                        if (a[row][col] != original) {
-                                return false;
-                        }
-                }
-                return true;
-        }
-        int col = 0, row;
-        for (row = 1; row < rows; ++row) {
-                if (!matches(row, col)) return false;
-        }
-        row = 0;
-        for (col = 0; col < *cols; ++col) {
-                if (!matches(row, col)) return false;
+        int const len = *cols - 1;
+        for (int row = 1; row < rows; ++row) {
+                if (memcmp(a[row - 1], &a[row][1], len * sizeof **a) != 0)
+                        return false;
         }
         return true;
 }
