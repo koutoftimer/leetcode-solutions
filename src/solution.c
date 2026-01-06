181b8ceb9ef2af63fdac63818fa2eeabaf5528dd
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-06 19:35:58 +0200

    O(N) time O(1) space: count sort

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,35 @@
 #include "solution.h"
 
-// definition goes here
+static bool
+matches(char* word, int* target)
+{
+        int count[26] = {0};
+        while (*word) {
+                count[*word++ - 'a']++;
+        }
+        for (int i = 0; i < 26; ++i) {
+                if (count[i] < target[i]) return false;
+        }
+        return true;
+}
+
+char*
+shortestCompletingWord(char* license, char** words, int size)
+{
+        int target[26] = {0};
+        for (; *license; ++license) {
+                if (!isalpha(*license)) continue;
+                target[tolower(*license) - 'a']++;
+        }
+
+        char* res = NULL;
+        int len;
+        for (int i = 0; i < size; ++i) {
+                int const new_len = strlen(words[i]);
+                if ((!res || new_len < len) && matches(words[i], target)) {
+                        len = new_len;
+                        res = words[i];
+                }
+        }
+        return res;
+}
