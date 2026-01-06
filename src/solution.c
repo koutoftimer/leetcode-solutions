ac79f00caa1759b931a2867d251fe6f2428aa742
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-06 18:36:30 +0200

    O(N) time O(1) space: inplace DP

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,11 @@
 #include "solution.h"
 
-// definition goes here
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+int
+minCostClimbingStairs(int* cost, int size)
+{
+        for (int i = 2; i < size; ++i) {
+                cost[i] += MIN(cost[i - 1], cost[i - 2]);
+        }
+        return MIN(cost[size - 1], cost[size - 2]);
+}
