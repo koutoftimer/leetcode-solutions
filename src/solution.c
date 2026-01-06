0b1889037e3b9f9433c62a810304d7dacb6a892b
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-06 16:28:10 +0200

    solved: tail recursion

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,10 @@
 #include "solution.h"
 
-// definition goes here
+bool
+isOneBitCharacter(int* bits, int bitsSize)
+{
+        if (bitsSize == 1) return true;
+        if (bitsSize == 0) return false;
+        if (bits[0] == 0) return isOneBitCharacter(bits + 1, bitsSize - 1);
+        return isOneBitCharacter(bits + 2, bitsSize - 2);
+}
