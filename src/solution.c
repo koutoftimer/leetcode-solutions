ee4a5ffca86784949fd149b7573f41f5ec140afe
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 02:56:39 +0200

    solved

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,21 @@
 #include "solution.h"
 
-// definition goes here
+int*
+numberOfLines(int* widths, int widthsSize, char* s, int* returnSize)
+{
+        int* res   = malloc(2 * sizeof *res);
+        int* lines = &res[0];
+        int* width = &res[1];
+        *lines = 1, *width = 0;
+        for (; *s; ++s) {
+                int const size = widths[*s - 'a'];
+                if (*width + size <= 100) {
+                        *width += size;
+                } else {
+                        *width = size;
+                        (*lines)++;
+                }
+        }
+        *returnSize = 2;
+        return res;
+}
