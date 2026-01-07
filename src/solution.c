cb6ce88108bc216a8dd927830631deda5319a7df
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 14:34:02 +0200

    solved

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,19 @@
 #include "solution.h"
 
-// definition goes here
+int**
+flipAndInvertImage(int** image, int rows, int* cols, int* returnSize,
+                   int** returnColumnSizes)
+{
+        int** res = malloc(rows * sizeof *res);
+        for (int row = 0; row < rows; ++row) {
+                res[row] = malloc(*cols * sizeof **res);
+                int size = 0;
+                for (int col = *cols - 1; col >= 0; --col) {
+                        res[row][size++] = image[row][col] ^ 1;
+                }
+        }
+        *returnSize        = rows;
+        *returnColumnSizes = malloc(rows * sizeof *returnColumnSizes);
+        memcpy(*returnColumnSizes, cols, rows * sizeof *cols);
+        return res;
+}
