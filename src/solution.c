2ab9184695aa50300cba121f82a0f6997121e545
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-08 00:20:39 +0200

    O(N^2) time O(1) space

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,29 @@
 #include "solution.h"
 
-// definition goes here
+int
+projectionArea(int** grid, int n, int* ys)
+{
+        int xy_projection = 0;
+        for (int x = 0; x < n; ++x) {
+                for (int y = 0; y < n; ++y) {
+                        xy_projection += (bool)grid[x][y];
+                }
+        }
+        int xz_projection = 0;
+        for (int x = 0; x < n; ++x) {
+                int max = grid[x][0];
+                for (int y = 1; y < n; ++y) {
+                        if (grid[x][y] > max) max = grid[x][y];
+                }
+                xz_projection += max;
+        }
+        int yz_projection = 0;
+        for (int y = 0; y < n; ++y) {
+                int max = grid[0][y];
+                for (int x = 1; x < n; ++x) {
+                        if (grid[x][y] > max) max = grid[x][y];
+                }
+                yz_projection += max;
+        }
+        return xy_projection + xz_projection + yz_projection;
+}
