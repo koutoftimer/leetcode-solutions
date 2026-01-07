d28bc50ce95a72cf1b6d4a7231bd17d13227719c
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 16:03:09 +0200

    O(N) time O(1) space: inplace reduce

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,22 @@
 #include "solution.h"
 
-// definition goes here
+static void
+reduce(char* s)
+{
+        char *in = s, *out = s;
+        for (; *in; ++in) {
+                if (*in == '#') {
+                        out -= out > s;
+                } else {
+                        *out++ = *in;
+                }
+        }
+        *out = '\0';
+}
+bool
+backspaceCompare(char* s, char* t)
+{
+        reduce(s);
+        reduce(t);
+        return strcmp(s, t) == 0;
+}
