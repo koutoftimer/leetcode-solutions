dfa87a44361e9c77df412bf067f6d4571a270499
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 16:28:05 +0200

    O(N) time O(1) space: two pointers

--- src/solution.c
+++ src/solution.c
@@ -1,22 +1,31 @@
 #include "solution.h"
 
-static void
-reduce(char* s)
+static char*
+next(char* s, char* it)
 {
-        char *in = s, *out = s;
-        for (; *in; ++in) {
-                if (*in == '#') {
-                        out -= out > s;
+        int skip = 0;
+        while (it >= s) {
+                if (*it == '#') {
+                        --it, ++skip;
+                } else if (skip) {
+                        --it, --skip;
                 } else {
-                        *out++ = *in;
+                        break;
                 }
         }
-        *out = '\0';
+        return it;
 }
 bool
 backspaceCompare(char* s, char* t)
 {
-        reduce(s);
-        reduce(t);
-        return strcmp(s, t) == 0;
+        int const s_len = strlen(s);
+        int const t_len = strlen(t);
+        char* s_it      = next(s, s + s_len - 1);
+        char* t_it      = next(t, t + t_len - 1);
+        while (s_it >= s && t_it >= t) {
+                if (*s_it != *t_it) return false;
+                s_it = next(s, s_it - 1);
+                t_it = next(t, t_it - 1);
+        }
+        return s_it < s && t_it < t;
 }
