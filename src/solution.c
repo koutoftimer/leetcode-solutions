e3ffeba1f4b4b259c9139d80001e162c89f50f7d
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 17:59:02 +0200

    O(logN) time O(1) space

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,20 @@
 #include "solution.h"
 
-// definition goes here
+int
+binaryGap(int n)
+{
+        int max  = 0;
+        int last = -1;
+        int pos  = 0;
+        while (n) {
+                if (n & 1) {
+                        if (last != -1 && pos - last > max) {
+                                max = pos - last;
+                        }
+                        last = pos;
+                }
+                n /= 2;
+                ++pos;
+        }
+        return max;
+}
