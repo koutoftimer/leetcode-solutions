536a7127bb4e43b2172fab7ee4f54041e5ac71b1
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 17:38:20 +0200

    solved

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,28 @@
 #include "solution.h"
 
-// definition goes here
+bool
+lemonadeChange(int* bills, int billsSize)
+{
+        int count[21] = {0};
+        for (int i = 0; i < billsSize; ++i) {
+                switch (bills[i]) {
+                case 5: {
+                        count[5]++;
+                } break;
+                case 10: {
+                        if (!count[5]--) return false;
+                        count[10]++;
+                } break;
+                case 20: {
+                        if (count[5] && count[10]) {
+                                count[5]--, count[10]--;
+                        } else if (count[5] >= 3) {
+                                count[5] -= 3;
+                        } else {
+                                return false;
+                        }
+                } break;
+                }
+        }
+        return true;
+}
