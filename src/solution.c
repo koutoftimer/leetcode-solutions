8028505e5d42ff4c805cf314fff9e3bbe1a21869
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 02:18:44 +0200

    HashSet

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,42 @@
 #include "solution.h"
 
-// definition goes here
+char* morze_codes[] = {
+    ".-",   "-...", "-.-.", "-..",  ".",   "..-.", "--.",  "....", "..",
+    ".---", "-.-",  ".-..", "--",   "-.",  "---",  ".--.", "--.-", ".-.",
+    "...",  "-",    "..-",  "...-", ".--", "-..-", "-.--", "--..",
+};
+
+static ulong
+hash(char* word)
+{
+        ulong code = 1;
+        while (*word) {
+                char* sequence = morze_codes[*word++ - 'a'];
+                while (*sequence) {
+                        code <<= 1;
+                        code |= *sequence++ == '.';
+                }
+        }
+        return code;
+}
+
+enum { CAPACITY = 211 };
+
+int
+uniqueMorseRepresentations(char** words, int size)
+{
+        ulong hash_set[CAPACITY] = {0};
+        int hash_set_size        = 0;
+        for (int i = 0; i < size; ++i) {
+                ulong word_hash = hash(words[i]);
+                int index       = word_hash % CAPACITY;
+                while (hash_set[index] && hash_set[index] != word_hash) {
+                        index = (index + 1) % CAPACITY;
+                }
+                if (!hash_set[index]) {
+                        hash_set_size++;
+                        hash_set[index] = word_hash;
+                }
+        }
+        return hash_set_size;
+}
