7be9e0c83713c4b2a895733d76013588bf9ce0fe
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 18:26:30 +0200

    dfs with stack, 100% time and memory

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,37 @@
 #include "solution.h"
 
-// definition goes here
+int stack[200];
+int stack_size;
+
+#define is_leaf(root) (!(root)->left && !(root)->right)
+
+static void
+traverse(struct TreeNode* root)
+{
+        if (!root) return;
+        if (is_leaf(root)) {
+                stack[stack_size++] = root->val;
+        }
+        traverse(root->left);
+        traverse(root->right);
+}
+
+static bool
+reverse(struct TreeNode* root)
+{
+        if (!root) return true;
+        if (is_leaf(root)) {
+                if (!stack_size) return false;
+                int const value = stack[--stack_size];
+                if (value != root->val) return false;
+        }
+        return reverse(root->right) && reverse(root->left);
+}
+
+bool
+leafSimilar(struct TreeNode* root1, struct TreeNode* root2)
+{
+        stack_size = 0;
+        traverse(root1);
+        return reverse(root2) && !stack_size;
+}
