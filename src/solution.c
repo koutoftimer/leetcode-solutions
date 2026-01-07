04268b0f787cc798ecc628d602a433f957bfc896
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 15:20:54 +0200

    solved: restricting corner points

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,20 @@
 #include "solution.h"
 
-// definition goes here
+#define SWAP(a, b)                         \
+        do {                               \
+                __typeof(a) __tmp = a;     \
+                a                 = b;     \
+                b                 = __tmp; \
+        } while (0)
+bool
+isRectangleOverlap(int* a, int a_len, int* b, int b_len)
+{
+        struct Rect {
+                int x_start, y_start, x_stop, y_stop;
+        };
+        struct Rect const* left  = (struct Rect*)a;
+        struct Rect const* right = (struct Rect*)b;
+        if (left->x_start > right->x_start) SWAP(left, right);
+        return right->x_start < left->x_stop && right->y_start < left->y_stop &&
+               right->y_stop > left->y_start;
+}
