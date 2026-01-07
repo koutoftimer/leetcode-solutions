f626f597edc99621a69133a0e2cafcb466cfbc5c
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 13:52:45 +0200

    O(N) time O(N) space: utilizes arena to reduce amount of allocations

--- src/solution.c
+++ src/solution.c
@@ -1,3 +1,26 @@
 #include "solution.h"
 
-// definition goes here
+int**
+largeGroupPositions(char* s, int* returnSize, int** returnColumnSizes)
+{
+        int const len   = strlen(s);
+        int max_chunks  = (len + 2) / 3;
+        int** res       = malloc(max_chunks * sizeof *res);
+        int size        = 0;
+        int (*arena)[2] = malloc(max_chunks * sizeof(*arena));
+        int arena_size  = 0;
+        char *l = s, *r = s;
+        while (*r) {
+                while (*r && *r == *l) ++r;
+                if (r - l > 2) {
+                        res[size]      = arena[arena_size++];
+                        res[size][0]   = l - s;
+                        res[size++][1] = r - s - 1;
+                }
+                l = r;
+        }
+        *returnSize        = size;
+        *returnColumnSizes = malloc(size * sizeof *returnColumnSizes);
+        for (int i = 0; i < size; ++i) (*returnColumnSizes)[i] = 2;
+        return res;
+}
