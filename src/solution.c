70da206e409544d8ddb0ed36d6b8bbb118026887
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 17:07:11 +0200

    Improved to reach 100% by time and memory

--- src/solution.c
+++ src/solution.c
@@ -8,23 +8,26 @@ buddyStrings(char* s, char* goal)
         if (s_len != goal_len) {
                 return false;
         }
-        int count_s[26]    = {0};
-        int count_goal[26] = {0};
-        for (int i = 0; i < s_len; ++i) {
-                count_s[s[i] - 'a']++;
-                count_goal[goal[i] - 'a']++;
-        }
-        if (memcmp(count_s, count_goal, sizeof(count_s)) != 0) {
+
+        if (memcmp(s, goal, s_len) == 0) {
+                uint bit_set = 0;
+                while (*s) {
+                        uint const mask = 1 << (*s++ - 'a');
+                        if (bit_set & mask) {
+                                return true;
+                        }
+                        bit_set |= mask;
+                }
                 return false;
         }
-        int diff = 0;
-        while (*s) {
-                diff += *s++ != *goal++;
+
+        int stack[3];
+        int size = 0;
+        for (int i = 0; size < 3 && i < s_len; ++i) {
+                if (s[i] != goal[i]) {
+                        stack[size++] = i;
+                }
         }
-        if (diff == 2) return true;
-        if (diff != 0) return false;
-        for (int i = 0; i < 26; ++i) {
-                if (count_s[i] > 1) return true;
-        }
-        return false;
+        return size == 2 && s[stack[0]] == goal[stack[1]] &&
+               s[stack[1]] == goal[stack[0]];
 }
