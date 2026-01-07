679c8ee9f88720f8447ce8dfb6d8496ab7e5d8f9
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-07 03:32:55 +0200

    Brute force: area by 3 points in integers

--- src/solution.c
+++ src/solution.c
@@ -3,28 +3,28 @@
 double
 largestTriangleArea(int** points, int pointsSize, int* pointsColSize)
 {
-        long double area(int a, int b, int c)
+        int area(int a, int b, int c)
         {
-#define square(a) (a) * (a)
-#define line_length(a, b)                           \
-        sqrtl(square(points[a][0] - points[b][0]) + \
-              square(points[a][1] - points[b][1]))
-                long double const ab = line_length(a, b);
-                long double const ac = line_length(a, c);
-                long double const bc = line_length(b, c);
-                long double const s  = (ab + ac + bc) / 2;
-                return s * (s - ab) * (s - ac) * (s - bc);
+                int const x1 = points[a][0];
+                int const x2 = points[b][0];
+                int const x3 = points[c][0];
+                int const y1 = points[a][1];
+                int const y2 = points[b][1];
+                int const y3 = points[c][1];
+                int const res =
+                    x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+                return abs(res);
         }
-        long double max = -1;
+        int max = -1;
         for (int a = 0; a < pointsSize; ++a) {
                 for (int b = a + 1; b < pointsSize; ++b) {
-                        for (int c = b = 1; c < pointsSize; ++c) {
-                                long double current = area(a, b, c);
+                        for (int c = b + 1; c < pointsSize; ++c) {
+                                long current = area(a, b, c);
                                 if (current > max) {
                                         max = current;
                                 }
                         }
                 }
         }
-        return sqrtl(max);
+        return sqrt(0.5 * max);
 }
