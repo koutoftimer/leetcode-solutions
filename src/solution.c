e6809ae35605f9744f4bf8cefbffb8ae0ead1b19
Ruslan Kovtun <koutoftimer@gmail.com>
2026-01-09 02:00:45 +0200

    Based on disscussion at
    https://leetcode.com/problems/max-dot-product-of-two-subsequences/solutions/7476484/dp3ms-beats-100-by-anwendeng-0vu7/

--- src/solution.c
+++ src/solution.c
@@ -1,7 +1,19 @@
 #include "solution.h"
 
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
+static inline int
+min(int a, int b)
+{
+        if (a < b) return a;
+        return b;
+}
+
+static inline int
+max(int a, int b)
+{
+        if (a > b) return a;
+        return b;
+}
+
 #define SWAP(a, b)                         \
         do {                               \
                 __typeof(a) __tmp = a;     \
@@ -12,71 +24,32 @@
 int
 maxDotProduct(int* a, int a_len, int* b, int b_len)
 {
-        if (a_len < b_len) return maxDotProduct(b, b_len, a, a_len);
+        // we want B to be longer for better cache utilization
+        if (a_len > b_len) return maxDotProduct(b, b_len, a, a_len);
 
-        int max_pair = a[0] * b[0];
-        for (int i = 0; i < a_len; ++i) {
-                for (int j = 0; j < b_len; ++j) {
-                        int const product = a[i] * b[j];
-                        if (product > max_pair) max_pair = product;
-                }
-        }
-        if (max_pair <= 0) {
-                return max_pair;
+        int prev_mem[500], current_mem[500];           // allocate on the stack
+        int *prev = prev_mem, *current = current_mem;  // decay into pointer
+
+        current[0] = a[0] * b[0];
+        for (uint16_t j = 1; j != b_len; ++j) {
+                current[j] = max(current[j - 1], a[0] * b[j]);
         }
 
-        // min([start;stop)) for b[i]
-        int min[b_len][b_len + 1];
-        memset(min, 0, sizeof(min));
-        // max([start;stop)) for b[i]
-        int max[b_len][b_len + 1];
-        memset(max, 0, sizeof(max));
-        for (int start = 0; start < b_len; ++start) {
-                max[start][start] = INT_MIN;
-                min[start][start] = INT_MAX;
-                for (int stop = start + 1; stop <= b_len; ++stop) {
-                        max[start][stop] =
-                            MAX(b[stop - 1], max[start][stop - 1]);
-                        min[start][stop] =
-                            MIN(b[stop - 1], min[start][stop - 1]);
+        for (uint16_t i = 1; i != a_len; ++i) {
+                SWAP(current, prev);
+
+                int const ai = a[i];
+                current[0]   = max(prev[0], ai * b[0]);
+                for (uint16_t j = 1; j != b_len; ++j) {
+                        int const option1 = ai * b[j];
+                        int const option2 = prev[j - 1] + option1;
+                        int const option3 = current[j - 1];
+                        int const option4 = prev[j];
+                        // reduce amount of comparisons
+                        current[j] =
+                            max(max(option1, option2), max(option3, option4));
                 }
         }
 
-        // dp(i, [start; stop))
-        int dp_prev_mem[b_len + 1][b_len + 1];
-        // zero means we can't improve total sum, only values > 0 counts in
-        // total sum
-        memset(dp_prev_mem, 0, sizeof dp_prev_mem);
-        int dp_mem[b_len + 1][b_len + 1];
-        memset(dp_mem, 0, sizeof dp_mem);
-
-        int (*dp_prev)[b_len + 1] = dp_prev_mem;
-        int (*dp)[b_len + 1]      = dp_mem;
-
-        for (int i = a_len - 1; i >= 0; --i) {
-                for (int start = 0; start < b_len; ++start) {
-                        int dp_max = INT_MIN;
-                        for (int stop = start + 1; stop <= b_len; ++stop) {
-                                int value = dp_prev[start][stop];  // skip[i]
-                                int const max_dp = a[i] * max[start][stop] +
-                                                   dp_prev[stop][stop];
-                                if (value < max_dp) value = max_dp;
-
-                                int const min_dp = a[i] * min[start][stop] +
-                                                   dp_prev[stop][stop];
-                                if (value < min_dp) value = min_dp;
-                                dp[start][stop] = value;
-                                if (value > dp_max) dp_max = value;
-                        }
-                        dp[start][start] = dp_max;
-                }
-                SWAP(dp, dp_prev);
-        }
-        SWAP(dp, dp_prev);
-
-        int ans = 0;
-        for (int i = 1; i <= b_len; ++i) {
-                if (ans < dp[0][i]) ans = dp[0][i];
-        }
-        return dp[0][0];
+        return current[b_len - 1];
 }
